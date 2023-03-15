08d297d7e33edb40622bf017a613f8cce2645675
EC2 Default User <ec2-user@ip-172-31-75-30.ec2.internal>
2023-03-15 17:32:52 +0000

    feat: created code to return its inputs

---
+++ exercicio0.py
@@ -0,0 +1,11 @@
+#0 - Peça para  o usuário entrar com dois valores (primeiro e último).
+#Faça a contagem entre esses números.
+#Caso o último for menor que  o primeiro faça a contagem decrescente.
+#Caso o último número for maior que o primeiro faça a contagem crescente.
+
+valorA = input("Insira o primeiro Número ")
+valorB = input("Insira o segundo Número ")
+
+print(valorA, valorB)
+
+
